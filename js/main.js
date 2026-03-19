46a941ebb7e058a3e3047511832e59a664e9af8e
MUHAMMED FAISAN <azeezfaizan@gmail.com>
2026-03-19 22:06:24 +0400

    yes

--- js/main.js
+++ js/main.js
@@ -117,6 +117,27 @@
             }
         }
     });
+
+    var swiper = new Swiper(".client-slider", {
+  loop: true,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 }
+  }
+});
     
 })(jQuery);
 
